87cbd8aa1742fe138e048aae730a38181f64814c
Anup1519 <anupraj1519@gmail.com>
2022-01-05 16:10:16 +0530

    Functional

--- src/functionalinterface/WellWisher.java
+++ src/functionalinterface/WellWisher.java
@@ -3,10 +3,15 @@ package functionalinterface;
 import functionalinterface.HindiGreeting;
 
 public class WellWisher {
+    public static void wish(Greeting greeting){  //Greeting interface is a parameter
+        greeting.greet();//that class greet method call
+    }
     public static void main(String[] args) {
         Greeting hg=new HindiGreeting();
-        hg.greet();
+        //hg.greet();
+        wish(hg);
         Greeting he=new EnglishGreeting();
-        he.greet();
+        //he.greet();
+        wish(he);
     }
 }
